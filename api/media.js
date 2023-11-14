ecd680bd40b289402040261e85529de4e7eb89eb
Sam Meester <sammeester1@gmail.com>
2023-11-14 15:23:44 -0800

    active film and book search

---
+++ api/media.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../appService');
+
+const root = 'media/'
+
+// All these routes fall under views/media
+
+router.post("/search", async (req, res) => {
+    try {
+        
+        // Only search if query is longer than 3 chars
+        if (req.body.search.length < 3) {
+            return res.send('<div id="search-results">Search for more than three characters.</div>');
+        }
+
+        // Set up table
+        let tableHtml = '<tbody id="search-results" class="">\n';
+
+        // Books
+        const book_data = await db.execute("SELECT * FROM \"Book\" WHERE \"title\" LIKE \'\%" + req.body.search + "\%\'");
+
+        // Deal with book data
+        for (let i = 0; i < book_data.length; i++) {
+            const title = book_data[i][1]
+            const date = book_data[i][2]
+
+            tableHtml += `<tr><td>Book</td><td>${title}</td><td>${date}</td></tr>\n`;
+        }
+
+
+        // Films
+        const film_data = await db.execute("SELECT * FROM \"Film\" WHERE \"title\" LIKE \'\%" + req.body.search + "\%\'");
+
+        // Deal with film data
+        for (let i = 0; i < film_data.length; i++) {
+            const title = film_data[i][1]
+            const date = film_data[i][2]
+
+            tableHtml += `<tr><td>Film</td><td>${title}</td><td>${date}</td></tr>\n`;
+        }
+
+        
+
+        // Music
+        // TODO
+
+        // End table
+        tableHtml += '</tbody>';
+
+        // Send results
+        return res.send(tableHtml);
+    } catch (error) {
+        console.error("Error fetching data:", error)
+        res.status(500).json({ error: "An error occurred while fetching data." });
+    }
+});
+
+module.exports = router;
